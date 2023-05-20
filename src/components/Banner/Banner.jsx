4c621608a44be0ad13a41fca4741b2a6be774518
RianOntu <rianontu13@gmail.com>
2023-05-20 23:30:32 +0600

    created banner section

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Banner.css'
+
+const Banner = () => {
+    return (
+        <div className='banner'>
+        <img src="1.jpg" alt="" />
+        <div className="container">
+            <div className='description'>
+            <h1 >Explore various indian <br /> chefs now</h1>
+        <button className='btn btn-primary'>Explore</button>
+            </div>
+    
+        </div>
+            
+        </div>
+    );
+};
+
+export default Banner;
